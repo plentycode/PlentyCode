0b3443ddeb56d824481396e402d50463433adc15
Carlos Iván Mercado <carlos.mercado@plentycode.com>
2015-03-20 12:42:12 -0500

    adding splash site

---
+++ Splash/js/splash.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    if (window.location.hash) {
+        setTimeout('showContent()', 200);
+    }
+    $('body').click(function (e) {
+        showContent();
+    });
+    $('.menu-bottom, .plenty-info-box, .plenty-social').click(function (e) {
+        e.stopPropagation();
+    });
+    $('.plenty-icon').click(function () {
+        $('.plenty-icon').removeClass('selected');
+        $(this).addClass('selected');
+    });
+    if (screen.width <= 699) {
+        $('body').addClass('mobile');
+    }
+});
+
+function showContent() {
+    $('.menu-bottom').toggleClass('open');
+    $('.plenty-logo').toggleClass('logo-to-corner');
+    $('.plenty-info-box').toggleClass('open');
+    $('.plenty-social').toggleClass('hidden');
+};
