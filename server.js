f3ff9b47d062a89780739d82b2feb98e74ba6410
Iván Mercado <civan.cim@gmail.com>
2015-05-10 18:46:45 -0500

    updated media_content paths

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bo
 //routes
 app.use(express.static(__dirname + '/release/'));
 
-//application port it takes localhost:5000 when is running locally
-app.set('port', (process.env.PORT || 5000));
+//application port it takes localhost:2000 when is running locally
+app.set('port', (process.env.PORT || 2000));
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
